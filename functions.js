84302f6f9184fb5a09002313d5a2268e84e8310d
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-06-16 12:27:57 -0700

    added some function

--- functions.js
+++ functions.js
@@ -1,24 +1,24 @@
-/*
-For this first set of functions, assume the input array looks like this:
 
-const petsArray = [
-    { name: 'spot', type: 'dog' },
-    { name: 'rover', type: 'dog' },
-    { name: 'jumpy', type: 'frog' },
-    { name: 'einstein', type: 'cat' },
-];
+// For this first set of functions, assume the input array looks like this:
+
+// const petsArray = [
+//     { name: 'spot', type: 'dog' },
+//     { name: 'rover', type: 'dog' },
+//     { name: 'jumpy', type: 'frog' },
+//     { name: 'einstein', type: 'cat' },
+// ];
 
 
-/*
-OUTPUT: 
-[
-    { name: 'spot', type: 'dog' },
-    { name: 'rover', type: 'dog' },
 
-]*/
+// OUTPUT: 
+// [
+//     { name: 'spot', type: 'dog' },
+//     { name: 'rover', type: 'dog' },
+
+// ]
 
 export function getDogs(arr) {
-    return [];
+    return arr.filter((pet => pet.type === 'dog'));
 }
 
 /*
@@ -28,7 +28,7 @@ Output:
 */
 
 export function makeArrayOfNames(arr) {
-    return [];
+    return arr.map((pet) => pet.name);
 }
 
 /*
@@ -37,7 +37,8 @@ OUTPUT:
 */
 
 export function getNamesOfDogs(arr) {
-    return [];
+    return arr.filter((pet) => pet.type === 'dog').map((pet) => pet.name);
+    
 }
 
 /*
@@ -47,7 +48,7 @@ Output:
 */
 
 export function makeReversedArrayOfTypes(arr) {
-    return [];
+    return arr.map((pet) => pet.type).reverse(arr);
 }
 
 /*
@@ -62,7 +63,13 @@ Output:
 */
 
 export function makeSpanishLanguageArray(arr) {
-    return [];
+    return arr.map((pet) => {
+        return {
+            nombre: pet.name,
+            tipo: pet.type
+        };
+    });
+    
 }
 
 /*
@@ -76,7 +83,12 @@ Output:
 ]*/
 
 export function makeArrayWithIsHungry(arr) {
-     return []
+    return arr.map((pet) => {
+        return {
+            ...pet,
+            isHungry: true
+        };
+    });
 }
 
 /*
@@ -90,7 +102,13 @@ Output:
 ]*/
 
 export function makeShoutingArray(arr) {
-    return [];
+    return arr.map(pet=> 
+        ({
+            ...pet,
+            name: pet.name.toUpperCase(),
+            
+        })
+    );
 }
 
 
@@ -101,7 +119,7 @@ Output:
 */
 
 export function makeStringArray(arr) {
-    return [];
+    return arr.map((pet) => pet.name + pet.type);
 }
 
 /*
@@ -114,7 +132,7 @@ OUTPUT:
 */
 
 export function findByName(name, arr) {
-    return {};
+    return arr.filter((pet) => name === pet.name);
 }
 
 /*
